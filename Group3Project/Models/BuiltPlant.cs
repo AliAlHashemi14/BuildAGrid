c6d2fd8fb936dcb095e0f54de677ac5f68c35f09
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-08-25 10:56:24 -0400

    built project and database in Azure

---
+++ Group3Project/Models/BuiltPlant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Group3Project.Models
+{
+    public partial class BuiltPlant
+    {
+        public int Id { get; set; }
+        public string? FuelType { get; set; }
+        public int? NameplateCapacity { get; set; }
+        public int? RampRate { get; set; }
+        public bool? PowState { get; set; }
+    }
+}
---
+++ Group3Project/Models/EnergyDBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Group3Project.Models
+{
+    public partial class EnergyDBContext : DbContext
+    {
+        public EnergyDBContext()
+        {
+        }
+
+        public EnergyDBContext(DbContextOptions<EnergyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<BuiltPlant> BuiltPlants { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer($"Data Source={Secret.address};Initial Catalog=EnergyDB; User Id={Secret.userId}; Password={Secret.password};");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BuiltPlant>(entity =>
+            {
+                entity.Property(e => e.FuelType).HasMaxLength(255);
+
+                entity.Property(e => e.NameplateCapacity).HasColumnName("nameplateCapacity");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
